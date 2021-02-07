41a80d7513ac8201b33538e1b4148eba41a63a16
jeantiston <jean.tiston.work@gmail.com>
2021-02-07 19:49:37 +0800

    Added app styling and added top bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+import React from 'react';
+import TopBar from './components/TopBar'
+
+
+
 function App() {
   return (
     <div className="App">
+      <TopBar />
       Hello World
     </div>
   );
---
+++ src/components/TopBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+
+import profilePhoto from '../assets/profile-photo.png'
+import logo from '../assets/gridxplanner-logo.svg'
+
+import TopBarStyles from '../styles/topbar.module.css'
+
+const TopBar = () => {
+    return (
+        <div className={TopBarStyles.grid}>
+            <div>
+                <img className={TopBarStyles.profilePhoto} src={profilePhoto} alt="profile" />
+            </div>
+            <div>
+                <img className={TopBarStyles.logo} src={logo} alt="GridXPlanner logo" />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faCog} size="2x" />
+            </div>
+        </div>
+    )
+}
+
+export default TopBar
