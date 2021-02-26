0e09d1a472ba47e2e289b3c2fa8b62b929676643
jeantiston <jean.tiston.work@gmail.com>
2021-02-26 21:14:38 +0800

    Added drag and drop

--- src/components/GridImage.js
+++ src/components/GridImage.js
@@ -1,17 +1,78 @@
-import React, {Fragment, useState, useRef} from 'react'
+import React, { useRef } from 'react'
 import { useDrag, useDrop } from 'react-dnd'
 import { Link } from 'react-router-dom'
 
 import ITEM_TYPE from '../data/types'
 import styles from '../styles/gridgallery.module.css'
 
-const GridImage = (props) => {
+const GridImage = ({ id, index, moveCard }) => {
     const ref = useRef(null)
     
+    const [{ handlerId }, drop] = useDrop({
+        accept: ITEM_TYPE,
+        collect(monitor) {
+            return {
+                handlerId: monitor.getHandlerId()
+            };
+        },
+        hover(item, monitor) {
+            if (!ref.current) {
+                return;
+            }
+            const dragIndex = item.index;
+            const hoverIndex = index;
+
+            // Don't replace items with themselves
+            if (dragIndex === hoverIndex) {
+                return;
+            }
+            // Determine rectangle on screen
+            const hoverBoundingRect = ref.current?.getBoundingClientRect();
+            // Get vertical middle
+            const hoverMiddleY =
+                (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+            // Determine mouse position
+            const clientOffset = monitor.getClientOffset();
+            // Get pixels to the top
+            const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+            // Only perform the move when the mouse has crossed half of the items height
+            // When dragging downwards, only move when the cursor is below 50%
+            // When dragging upwards, only move when the cursor is above 50%
+            // Dragging downwards
+            console.log("hoverClientY: ", hoverClientY)
+            console.log("hoverMiddleY: ", hoverMiddleY)
+            
+            // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+            //     return;
+            // }
+            // Dragging upwards
+            // if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+            //     return;
+            // }
+            // Time to actually perform the action
+            moveCard(dragIndex, hoverIndex);
+            // Note: we're mutating the monitor item here!
+            // Generally it's better to avoid mutations,
+            // but it's good here for the sake of performance
+            // to avoid expensive index searches.
+            item.index = hoverIndex;
+        }
+    })
+
+    const [{ isDragging }, drag] = useDrag({
+        item: { type: ITEM_TYPE, id, index },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging()
+        })
+    })
+
+    const opacity = isDragging ? 0 : 1
+    drag(drop(ref))
+
     return (
-        <div>
-            <Link to={"/edit/"+ props.id.toString()}>
-                    <img className={styles.gridCell} src={"https://picsum.photos/300?random=" + props.id.toString()} alt="random picsum" />
+        <div style={{opacity}} ref={ref} data-handler-id={handlerId}>
+            <Link to={"/edit/"+ id.toString()}>
+                    <img className={styles.gridCell} src={"https://picsum.photos/300?random=" + id.toString()} alt="random picsum" />
             </Link>
         </div>
     )
--- src/pages/GridGallery.js
+++ src/pages/GridGallery.js
@@ -1,42 +1,88 @@
-import React, {useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState, useCallback} from 'react'
+// import { Link } from 'react-router-dom'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import update from 'immutability-helper'
 
 import GridImage from '../components/GridImage'
-import GridSquare from '../components/GridSquare'
+// import GridSquare from '../components/GridSquare'
 import styles from '../styles/gridgallery.module.css'
 
 const GridGallery = () => {
-    const [gridItems, setGridItems] = useState([])
-    useEffect(() => {
-        let imgList = []
-        for (let i = 0 ; i <= 15 ; i++) {
-            imgList.push("https://picsum.photos/300?random=" + i.toString())
-        }
-        const items = imgList.map((img,i) => {
-            return (
-                <Link to={"/edit/"+ i.toString()} key={i}>
-                    <img className={styles.gridCell} src={img} alt={i} key={i} />
-                </Link>
-            )
-        })
+    const [images, setImages] = useState([
+        {
+            id: 1,
+            url: "https://picsum.photos/id/1/300"
+        },
+        {
+            id: 2,
+            url: "https://picsum.photos/id/2/300"
+        },
+        {
+            id: 3,
+            url: "https://picsum.photos/id/3/300"
+        },
+        {
+            id: 4,
+            url: "https://picsum.photos/id/4/300"
+        },
+        {
+            id: 5,
+            url: "https://picsum.photos/id/5/300"
+        },
+        {
+            id: 6,
+            url: "https://picsum.photos/id/6/300"
+        },
+        {
+            id: 7,
+            url: "https://picsum.photos/id/7/300"
+        },
+    ]);
 
-        setGridItems(items)
+    // const [gridItems, setGridItems] = useState([])
+    // useEffect(() => {
+    //     let imgList = []
+    //     for (let i = 0 ; i <= 15 ; i++) {
+    //         imgList.push("https://picsum.photos/300?random=" + i.toString())
+    //     }
+    //     const items = imgList.map((img,i) => {
+    //         return (
+    //             <Link to={"/edit/"+ i.toString()} key={i}>
+    //                 <img className={styles.gridCell} src={img} alt={i} key={i} />
+    //             </Link>
+    //         )
+    //     })
 
-    },[])
+    //     setGridItems(items)
 
+    // },[])
 
+    const moveCard = useCallback((dragIndex, hoverIndex) => {
+        const dragCard = images[dragIndex];
+        setImages(update(images, {
+            $splice: [
+                [dragIndex, 1],
+                [hoverIndex, 0, dragCard],
+            ],
+        }));
+    }, [images]);
 
+    const renderCard = (image, index) => {
+        return (
+                <GridImage key={image.id} index={index} id={image.id} moveCard={moveCard} />
+            );
+    };
 
     return (
         <DndProvider backend={HTML5Backend}>
             <div className={styles.grid}>
-                <GridSquare pos="1">
+                {/* <GridSquare pos="1"> */}
                     {/* <p>image</p> */}
-                    <GridImage id="123" />
-                </GridSquare>
-                { gridItems }
+                {/* <GridImage id="123" /> */}
+                {/* </GridSquare> */}
+                {/* { gridItems } */}
+                { images.map((card, i) => renderCard(card, i)) }
             </div>
         </DndProvider>
     )
