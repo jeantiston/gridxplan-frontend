ac0dbfbd76663f0c5a908b7e75b18edd7889eab2
jeantiston <jean.tiston.work@gmail.com>
2021-03-10 16:35:10 +0800

    Removed unused code from PostBar

--- src/components/PostBar.js
+++ src/components/PostBar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faEdit, faComment } from '@fortawesome/free-solid-svg-icons'
 
@@ -7,16 +6,7 @@ import styles from '../styles/postbar.module.css'
 
 const PostBar = ({ children, editSection, setEditSection }) => {
 
-    const location = useLocation()
-    const [, path, id] = location.pathname.split("/")
-
     const isActive = (currentPath) => {
-        // console.log(children)
-        // console.log(setEditSection)
-        // console.log(location.pathname.split("/")[1])
-        // if (path === currentPath) {
-        //     return styles.active
-        // }
 
         if( editSection && currentPath === 'edit' ){
             return [styles.menuText, styles.active].join(" ")
@@ -43,19 +33,15 @@ const PostBar = ({ children, editSection, setEditSection }) => {
             { children }
 
             <div className={styles.flexBottom} >
-                {/* <NavLink exact to={"/edit/" + id.toString()} activeClassName={styles.active} > */}
-                    <div className={styles.menuItem} onClick={() => setEditSection(true)} >
-                        <FontAwesomeIcon icon={faEdit} size="lg" className={ isActive("edit") } />
-                        <sub className={ isActive("edit") }>edit</sub>
-                    </div>
-                {/* </NavLink> */}
+                <div className={styles.menuItem} onClick={() => setEditSection(true)} >
+                    <FontAwesomeIcon icon={faEdit} size="lg" className={ isActive("edit") } />
+                    <sub className={ isActive("edit") }>edit</sub>
+                </div>
 
-                {/* <NavLink exact to={"/preview/" + id.toString()} activeClassName={styles.active} > */}
-                    <div className={styles.menuItem} onClick={() => setEditSection(false)} >
-                        <FontAwesomeIcon icon={faComment} size="lg" className={ isActive("preview") } />
-                        <sub className={ isActive("preview") }>comments</sub>
-                    </div>
-                {/* </NavLink> */}
+                <div className={styles.menuItem} onClick={() => setEditSection(false)} >
+                    <FontAwesomeIcon icon={faComment} size="lg" className={ isActive("preview") } />
+                    <sub className={ isActive("preview") }>comments</sub>
+                </div>
             </div>
         </div>
 
